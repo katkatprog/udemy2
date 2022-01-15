9407aab80aba2d293938bb0ea893f8b00f35ce63
katkatprog <80642234+katkatprog@users.noreply.github.com>
2022-01-15 19:54:39 +0900

    Update settings.py

--- backend/django_project/django_project/settings.py
+++ backend/django_project/django_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', #[added]
+    'rest_framework.authtoken',#[added]
     # 'app.apps.AppConfig' #[added]example
 ]
 
